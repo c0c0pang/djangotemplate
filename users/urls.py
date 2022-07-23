d62eea81d7f2d16b23961c312648d11df75ad8e1
c0c0pang <coc1789@naver.com>
2022-07-24 02:50:06 +0900

    장고를 이용한 로그인 로그아웃 회원가입 기능 구현

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 app_name = "user"
 urlpatterns = [
-    path("main", views.main_view, name="login"),
+    path("login", views.login_view, name="login"),
     path("signup", views.signup_view, name="signup"),
     path("logout", views.logout_view, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
+import imp
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-
+from .models import User
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
 # Create your views here.
 
 
-def main_view(request):
+def login_view(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
@@ -15,11 +18,6 @@ def main_view(request):
             return render(request, "home.html")
         else:
             print("인증실패")
-
-    return render(request, "users/main.html")
-
-
-def login_view(request):
     return render(request, "users/login.html")
 
 
@@ -29,4 +27,17 @@ def logout_view(request):
 
 
 def signup_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        re_password = request.POST["re_password"]
+        email = request.POST["email"]
+        res_data = {}
+        if password != re_password:
+            res_data['error'] = "비밀번호가 다름"
+        else:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+            return redirect("user:login")
+
     return render(request, "users/signup.html")
